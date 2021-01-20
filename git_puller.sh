da0504f9c9a99bb2ac2160398f737877fc9faeb5
dillonfeinman <dfeinma1@binghamton.edu>
2021-01-20 15:20:09 -0800

    added functionality to input username

--- git_puller.sh
+++ git_puller.sh
@@ -1,33 +1,40 @@
 #!/usr/bin/env bash
 
 DIR="$(pwd)"
+USER="$(echo $1)"
 
-if [ ! -d "$DIR"/repos ]
+if [ "$USER" == "" ]
 then
-    echo "Creating repos folder..."
-    mkdir repos
+    echo "Usage: sh git_puller.sh \"username\""
+else
+    if [ ! -d "$DIR"/repos ]
+    then
+        echo "Creating repos folder..."
+        mkdir repos
+    fi
+
+    if [ ! -d "$DIR"/repos/"$USER" ]
+    then
+        echo "Creating user: "$USER" folder"
+        mkdir repos/"$USER"
+    fi
+
+    cd "$DIR"/src
+
+    curl https://api.github.com/users/"$USER"/repos | grep -w 'git_url' > repos.txt
+
+    make
+
+    ./repo_splitter > split_repos.txt
+
+    cd "$DIR"/repos/"$USER"
+
+    for i in `cat "$DIR"/src/split_repos.txt`
+    do
+        git clone $i
+    done
+
+    cd "$DIR"/src
+
+    make clean
 fi
-
-
-cd src
-
-curl https://api.github.com/users/dillonfeinman/repos | grep -w 'git_url' > repos.txt
-
-make
-
-./repo_splitter > split_repos.txt
-
-cd ..
-
-cd repos
-
-for i in `cat "$DIR"/src/split_repos.txt`
-do
-    git clone $i
-done
-
-cd ..
-
-cd src
-
-make clean
